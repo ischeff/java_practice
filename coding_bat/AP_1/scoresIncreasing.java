f414ffe89145f79df6d74ce71bb7a05879598d1d
Ian Scheffler <ian.f.scheffler@gmail.com>
2022-09-28 00:16:03 -0400

    realized I needed a true return statement at the end

--- coding_bat/AP_1/scoresIncreasing.java
+++ coding_bat/AP_1/scoresIncreasing.java
@@ -1,5 +1,8 @@
 public boolean scoresIncreasing(int[] scores) {
-  for (int i = 1; i < scores[].length; i++){
-    return(if(int[i] <= int[i-1]));
+  for(int i = 1; i < scores.length; i++){
+    if(scores[i] < scores[i-1]){
+      return false;
+    }
   }
+  return true;
 }
